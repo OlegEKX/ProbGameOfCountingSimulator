5317354844cc3b00c88b7d99b61f4abe31590017
OleG484 <olegjarl@gmail.com>
2023-04-09 23:54:26 +0300

    Добавьте файлы проекта.

---
+++ ProbGameOfCountingSimulator/FormGame.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ProbGameOfCountingSimulator
+{
+    partial class Form_Schet
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_think = new System.Windows.Forms.Label();
+            this.textBox_answer = new System.Windows.Forms.TextBox();
+            this.pBar_time = new System.Windows.Forms.ProgressBar();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.lb_popytka = new System.Windows.Forms.Label();
+            this.tBox_level = new System.Windows.Forms.TextBox();
+            this.label_slozhost = new System.Windows.Forms.Label();
+            this.bt_plus = new System.Windows.Forms.Button();
+            this.bt_minus = new System.Windows.Forms.Button();
+            this.bt_mult = new System.Windows.Forms.Button();
+            this.bt_divs = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label_think
+            // 
+            this.label_think.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_think.Location = new System.Drawing.Point(12, 40);
+            this.label_think.Name = "label_think";
+            this.label_think.Size = new System.Drawing.Size(286, 66);
+            this.label_think.TabIndex = 0;
+            this.label_think.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox_answer
+            // 
+            this.textBox_answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_answer.Location = new System.Drawing.Point(304, 40);
+            this.textBox_answer.Multiline = true;
+            this.textBox_answer.Name = "textBox_answer";
+            this.textBox_answer.Size = new System.Drawing.Size(293, 66);
+            this.textBox_answer.TabIndex = 1;
+            // 
+            // pBar_time
+            // 
+            this.pBar_time.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pBar_time.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pBar_time.Location = new System.Drawing.Point(12, 113);
+            this.pBar_time.Maximum = 30;
+            this.pBar_time.Name = "pBar_time";
+            this.pBar_time.Size = new System.Drawing.Size(584, 23);
+            this.pBar_time.TabIndex = 9;
+            this.pBar_time.Tag = "progressBar";
+            this.pBar_time.Visible = false;
+            // 
+            // lb_time
+            // 
+            this.lb_time.AutoSize = true;
+            this.lb_time.Location = new System.Drawing.Point(284, 113);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(26, 13);
+            this.lb_time.TabIndex = 11;
+            this.lb_time.Text = "time";
+            this.lb_time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_popytka
+            // 
+            this.lb_popytka.Location = new System.Drawing.Point(13, 18);
+            this.lb_popytka.Name = "lb_popytka";
+            this.lb_popytka.Size = new System.Drawing.Size(55, 13);
+            this.lb_popytka.TabIndex = 12;
+            this.lb_popytka.Text = "попытка";
+            // 
+            // tBox_level
+            // 
+            this.tBox_level.Location = new System.Drawing.Point(495, 14);
+            this.tBox_level.Name = "tBox_level";
+            this.tBox_level.Size = new System.Drawing.Size(93, 20);
+            this.tBox_level.TabIndex = 7;
+            this.tBox_level.Text = "0";
+            this.tBox_level.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label_slozhost
+            // 
+            this.label_slozhost.AutoSize = true;
+            this.label_slozhost.Location = new System.Drawing.Point(380, 17);
+            this.label_slozhost.Name = "label_slozhost";
+            this.label_slozhost.Size = new System.Drawing.Size(109, 13);
+            this.label_slozhost.TabIndex = 13;
+            this.label_slozhost.Text = "Уровень сложности";
+            // 
+            // bt_plus
+            // 
+            this.bt_plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_plus.Location = new System.Drawing.Point(13, 148);
+            this.bt_plus.Name = "bt_plus";
+            this.bt_plus.Size = new System.Drawing.Size(142, 35);
+            this.bt_plus.TabIndex = 2;
+            this.bt_plus.Tag = "plus";
+            this.bt_plus.Text = "Сложение";
+            this.bt_plus.UseVisualStyleBackColor = true;
+            // 
+            // bt_minus
+            // 
+            this.bt_minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_minus.Location = new System.Drawing.Point(161, 148);
+            this.bt_minus.Name = "bt_minus";
+            this.bt_minus.Size = new System.Drawing.Size(142, 35);
+            this.bt_minus.TabIndex = 4;
+            this.bt_minus.Tag = "minus";
+            this.bt_minus.Text = "Сложение";
+            this.bt_minus.UseVisualStyleBackColor = true;
+            // 
+            // bt_mult
+            // 
+            this.bt_mult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_mult.Location = new System.Drawing.Point(309, 148);
+            this.bt_mult.Name = "bt_mult";
+            this.bt_mult.Size = new System.Drawing.Size(153, 35);
+            this.bt_mult.TabIndex = 5;
+            this.bt_mult.Tag = "mult";
+            this.bt_mult.Text = "Умножение";
+            this.bt_mult.UseVisualStyleBackColor = true;
+            // 
+            // bt_divs
+            // 
+            this.bt_divs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_divs.Location = new System.Drawing.Point(455, 148);
+            this.bt_divs.Name = "bt_divs";
+            this.bt_divs.Size = new System.Drawing.Size(142, 35);
+            this.bt_divs.TabIndex = 6;
+            this.bt_divs.Tag = "divs";
+            this.bt_divs.Text = "Деление";
+            this.bt_divs.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar.Location = new System.Drawing.Point(12, 189);
+            this.progressBar.Maximum = 30;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(583, 10);
+            this.progressBar.TabIndex = 14;
+            this.progressBar.Visible = false;
+            // 
+            // Form_Schet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 208);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.bt_divs);
+            this.Controls.Add(this.bt_mult);
+            this.Controls.Add(this.bt_minus);
+            this.Controls.Add(this.bt_plus);
+            this.Controls.Add(this.label_slozhost);
+            this.Controls.Add(this.tBox_level);
+            this.Controls.Add(this.lb_popytka);
+            this.Controls.Add(this.lb_time);
+            this.Controls.Add(this.pBar_time);
+            this.Controls.Add(this.textBox_answer);
+            this.Controls.Add(this.label_think);
+            this.Name = "Form_Schet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тренажер устного счета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_think;
+        private System.Windows.Forms.TextBox textBox_answer;
+        private System.Windows.Forms.ProgressBar pBar_time;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.Label lb_popytka;
+        private System.Windows.Forms.TextBox tBox_level;
+        private System.Windows.Forms.Label label_slozhost;
+        private System.Windows.Forms.Button bt_plus;
+        private System.Windows.Forms.Button bt_minus;
+        private System.Windows.Forms.Button bt_mult;
+        private System.Windows.Forms.Button bt_divs;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ ProbGameOfCountingSimulator/FormGame.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProbGameOfCountingSimulator
+{
+    public partial class Form_Schet : Form
+    {
+        System.Diagnostics.Stopwatch t = new System.Diagnostics.Stopwatch();
+        int count = 0; //количество попыток
+        int min = 1;
+        int i = 0; //для отображения секунд на форме
+        int max = 10; //уровень сложности задачи 10 - двухзначные числа, 100 - трехзначные числа
+        Random rnd = new Random();
+        string answer;
+        int count_max = 3; // для прогресс бара максимальное количество попыток
+        string mode = ""; // режим, что будет выполняться: сложение, вычитание, деление
+        bool plus = true,
+            minus = true,
+            mult = true,
+            divs = true;
+
+        public Form_Schet()
+        {
+            InitializeComponent();
+            tBox_level.Text = max.ToString();
+            timer.Interval = 1000; // 1 секунда
+        }
+
+        
+        
+        private void start(string my_mode)
+        {
+            pBar_time.Visible = true;
+            pBar_time.Value = 0; // 30 секунд максимум
+            lb_popytka.Visible = true;
+            count = 0;
+
+
+
+            mode = my_mode;
+            //убираем все кнопки на время игры
+            bt_plus.Visible = false;
+            bt_minus.Visible = false;
+            bt_mult.Visible = false;
+            bt_divs.Visible = false;
+
+            progressBar.Visible = true;
+            textBox_answer.Enabled = true;
+            label_think.Enabled = true;
+            textBox_answer.Focus();
+            progressBar.Minimum = 0;
+            progressBar.Maximum = count_max;
+            progressBar.Value = 0;
+            textBox_answer.Text = "";
+            label_think.Text = "";
+            lb_popytka.Text = "Попытка " + count + " из " + count_max;
+
+            try
+            {
+                max = Convert.ToInt16(tBox_level.Text);
+            }
+            catch
+            {
+                max = 10;
+                tBox_level.Text = max.ToString();
+            }
+            think();
+        }
+
+        private void think()
+        {
+            int a, b;
+            i = 0;
+            a = rnd.Next(min, max + 1);
+            b = rnd.Next(min, max + 1);
+
+            //на время запускаем счетчик
+
+            timer.Start();
+            t.Start();
+            pBar_time.Value = 0;
+
+
+
+            if (mode == "plus")
+            {
+                label_think.Text = a.ToString() + " + " + b.ToString() + " = ";
+                answer = (a + b).ToString();
+            }
+
+            if (mode == "minus")
+            {
+                if (a < b) //чтобы не было отрицательных чисел, поменяем местами
+                {
+                    int x = a;
+                    a = b;
+                    b = x;
+                }
+                label_think.Text = a.ToString() + " - " + b.ToString() + " = "; // - = ALT+0150 на цифровой
+                answer = (a - b).ToString();
+            }
+
+            if (mode == "mult")
+            {
+                label_think.Text = a.ToString() + " * " + b.ToString() + " = ";
+                answer = (a * b).ToString();
+            }
+            if (mode == "divs")
+            {
+                // можно так выпендриться, чтобы не использовать целочисленное деление
+                label_think.Text = (a * b).ToString() + " ; " + b.ToString() + " = ";
+                answer = a.ToString();
+            }
+            count++;
+
+
+        }
+        private void game_over()
+        {
+            textBox_answer.Text = "Нет = " + answer;
+            done();
+        }
+
+        private void done()
+        {
+            lb_popytka.Visible = false;
+            label_think.Enabled = false;
+            textBox_answer.Enabled = false;
+            pBar_time.Visible = false;
+            progressBar.Visible = false;
+            bt_plus.Focus(); // передаем фокус на кнопку игры
+            count = 0;
+
+            // возвращаем кнопки на панель согласно условию
+            // если какое-то задание выполнено, кнопка становится неактивной
+            // убираем все кнопки на время игры
+            bt_plus.Visible = true;
+            bt_minus.Visible = true;
+            bt_mult.Visible = true;
+            bt_divs.Visible = true;
+
+            bt_plus.Enabled = plus;
+            bt_minus.Enabled = minus;
+            bt_mult.Enabled = mult;
+            bt_divs.Enabled = divs;
+            if (!plus && !minus && !mult && !divs)
+                MessageBox.Show("Ты прошел все уровни!", "Твоя победа и полное поражение компьютера");
+            t.Stop();
+
+        }
+
+        private void game_wins()
+        {
+            int x = rnd.Next(1, 4); // случайное сообщение, всплывающее при прохождении уровня
+            if (x == 1) MessageBox.Show("Считаешь верно!!!", "А ты молодец. Математик!");
+            if (x == 2) MessageBox.Show("Верно!!!", "А ты молодец. Математик!");
+            if (x == 3) MessageBox.Show("Ответ принят!!!", "А ты молодец. Математик!");
+            if (x == 4) MessageBox.Show("У тебя все получается!!!", "А ты молодец. Математик!");
+
+            // если все посчитали верно кнопка становится недоступна
+            if (mode == "plus") plus = false;
+            if (mode == "minus") minus = false;
+            if (mode == "mult") mult = false;
+            if (mode == "divs") divs = false;
+            done();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            pBar_time.Increment(1);
+            lb_time.Text = "время решения " + i.ToString();
+            i++;
+
+            if (pBar_time.Value == pBar_time.Maximum) // как только достигнет максимума
+            {
+                t.Stop(); // останавливает проверку
+                timer.Stop(); // останавливает таймер
+            }
+
+            if (timer.Enabled == false)
+            {
+                MessageBox.Show("Не справился с задачей!" + t.Elapsed.ToString(), "Думай быстрее ");
+                game_over();
+            }
+
+
+
+        }
+
+
+
+        private void textBox_answer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // если в текстовом поле был нажат Enter, то считаем пример
+            {
+                lb_popytka.Text = "Попытка " + count + " из " + count_max;
+                if (count <= count_max) progressBar.Value = count;
+                if (textBox_answer.Text == answer)
+                {
+                    if (count == count_max)
+                    {
+                        game_wins(); return;
+                    }
+                    textBox_answer.Text = "";
+                    think(); // к следующему примеру, если ответ верный
+                }
+                else game_over();
+            }    
+        }
+
+        private void button_Click(object sender, EventArgs e) // обработка нажатых кнопок скопом
+        {
+            start((((Button)sender).Tag).ToString());
+        }
+
+
+        
+    }
+}
